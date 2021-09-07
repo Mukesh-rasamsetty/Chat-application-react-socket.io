9d99529eedf65230d80c9d7846b5615d7ec4a711
Mukesh-rasamsetty <54362124+Mukesh-rasamsetty@users.noreply.github.com>
2021-09-07 17:20:44 +0530

    Add files via upload

---
+++ Client/src/components/Chat/Chat.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import queryString from 'query-string';
+import io from 'socket.io-client';
+import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
+
+let socket;
+
+export default function Chat({ location }) {
+    const [name, setName] = React.useState('');
+    const [room, setRoom] = React.useState('');
+    const [users, setUsers] = React.useState('');
+    const [message, setMessage] = React.useState([]);
+    const [messages, setMessages] = React.useState([]);
+    const ENDPOINT = "http://localhost:5000";
+
+    React.useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+        socket = io(ENDPOINT);
+        setName(name);
+        setRoom(room);
+        socket.emit('join', { name, room }, (error) => {
+            if (error) {
+                alert(error);
+            }
+        });
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
+    }, [ENDPOINT, location.search]);
+
+    React.useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        });
+        socket.on("roomData", ({ users }) => {
+            setUsers(users);
+        });
+    }, [messages, users]);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+        if (message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    };
+
+    return (
+        <div className='outerContainer'>
+            <div className='container'>
+                <InfoBar room={room} />
+                <Messages messages={messages} name={name} />
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+            </div>
+            <TextContainer users={users} />
+        </div>
+    );
+}
---
+++ Client/src/components/Join/Join.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Join.css';
+
+export default function Join() {
+    const [name, setName] = React.useState('');
+    const [room, setRoom] = React.useState('');
+
+    return (
+        <div className='joinOuterContainer'>
+            <div className='joinInnerContainer'>
+                <h1 className='heading'>Join Room</h1>
+                <div>
+                    <input
+                        placeholder='Name'
+                        className='joinInput'
+                        type='text'
+                        onChange={(event) => setName(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <input
+                        placeholder='Room'
+                        className='joinInput mt-20'
+                        type='text'
+                        onChange={(event) => setRoom(event.target.value)}
+                    />
+                </div>
+                <Link
+                    onClick={event => (!name || !room) ? event.preventDefault() : null}
+                    to={`/chat?name=${name}&room=${room}`}
+                >
+                    <button className='button mt-20' type='submit'>Sign In</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
